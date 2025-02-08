9d5478770ace498660d9d53ee189bbb635b25891
Aurelio Munoz Hijano <aurelio@sparanzza.com>
2025-02-08 23:39:45 +0100

    fixed await Process Event

--- MicroRabbitMq/MicroRabbit.infra.Bus/RabbitMQBus.cs
+++ MicroRabbitMq/MicroRabbit.infra.Bus/RabbitMQBus.cs
@@ -93,7 +93,7 @@ public class RabbitMqBus : IEventBus
 
         try
         {
-            await ProccessEvent(eventName, message);
+            _ = ProccessEvent(eventName, message);
         }
         catch (Exception e)
         {
